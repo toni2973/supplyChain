75d70ca0e5f204ce223e363c57078af59d7aefb6
toni2973 <442903110@qq.com>
2017-01-13 19:49:54 -0800

    firts

---
+++ suppluchain/src/main/java/com/nju/toni/supplychain/SuppluchainApplication.java
@@ -0,0 +1,19 @@
+package com.nju.toni.supplychain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class SuppluchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppluchainApplication.class, args);
+	}
+}
